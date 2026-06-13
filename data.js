0633beb76c7f168acd6a2cd990fc4219a99390b2
hishamparamboor-web <hishamparamboor@gmail.com>
2026-06-14 03:06:03 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-olive-pan-14",
+      "name": "OLIVE PAN 1.4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5067410935951964,
+        "pitch": -0.1167364520910148,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5576997840649849,
+          "pitch": -0.017879596071121995,
+          "rotation": 0,
+          "target": "1-olive-pan-24"
+        },
+        {
+          "yaw": -3.1152034853644253,
+          "pitch": -0.07148572806064024,
+          "rotation": 0,
+          "target": "3-wash-pan-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-olive-pan-24",
+      "name": "OLIVE PAN 2.4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1757999227095368,
+        "pitch": -0.06905562103677632,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17633017002978058,
+          "pitch": 0.011905912475263847,
+          "rotation": 0,
+          "target": "0-olive-pan-14"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bathroom-pan12",
+      "name": "bathroom pan1.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7548149210293786,
+        "pitch": -0.00037970979212431644,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2928586165596663,
+          "pitch": 0.01866975016829997,
+          "rotation": 0,
+          "target": "3-wash-pan-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wash-pan-12",
+      "name": "wash pan 1.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5823643210115366,
+        "pitch": 0.011360154235626752,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1179550238853224,
+          "pitch": 0.09609164713881668,
+          "rotation": 0,
+          "target": "2-bathroom-pan12"
+        },
+        {
+          "yaw": 0.2028854683946797,
+          "pitch": 0.023266066710892375,
+          "rotation": 0,
+          "target": "0-olive-pan-14"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "poopalam arif",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
